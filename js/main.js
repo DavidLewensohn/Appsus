a40e29d275d17ffb43ea43ec8e24ab14e476d5d0
DavidLewensohn <davidlewensohn@gmail.com>
2022-06-29 20:48:44 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
 // import { router } from "./router.js"
-<<<<<<< HEAD
 // import  mailApp  from "./apps/mail/views/mail-app.cmp.js"
 import  noteApp  from "./apps/note/views/note-app.cmp.js"
 
-=======
 import  mailApp  from "./apps/mail/views/mail-app.cmp.js"
 // import  keepApp  from "./apps/keep/views/keep-app.cmp.js"
 import mailListCmp from "./apps/mail/cmps/mail-list.cmp.js"
 import mailPreviewCmp from "./apps/mail/cmps/mail-preview.cmp.js";
->>>>>>> 81e627701418242e633e824a8a9b14c6182fccae
 
 
 const options = {
     template: `
-<<<<<<< HEAD
     <!-- <mail-app/> -->
     <note-app/>
     `,
     components: {
         // mailApp,
         noteApp,
-=======
     <mail-app/>
     <!-- <keep-app/> -->
     `,
@@ -28,7 +23,6 @@ const options = {
         mailApp,
         // mailList,
         // keepApp,
->>>>>>> 81e627701418242e633e824a8a9b14c6182fccae
     },
 };
 
